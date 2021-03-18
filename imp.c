cc3ef54c93518efa74ca4f22b7477c136eba8ea9
wooosh <12696312+wooosh@users.noreply.github.com>
2021-03-17 20:25:42 -0400

    initial commit

---
+++ imp.c
@@ -0,0 +1,99 @@
+// #pragma once
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include <imp.h>
+
+char *imp_error_str[] = {
+  [imp_success] = "Success",
+  [imp_eof] = "EOF",
+  [imp_unexpected_bracket] = "Unexpected Bracket"
+};
+
+struct imp_parser imp_init(imp_command_callback callback, FILE* stream) {
+  struct imp_parser p = { callback, stream };
+
+  p._levels = malloc(sizeof(size_t));
+  p._levels_cap = 1;
+  
+  p._levels[0] = 0;
+  p._levels_len = 1;
+
+  return p;
+}
+
+// TODO: split into multiple functions
+enum imp_error imp_next_command(struct imp_parser *p) {
+  ssize_t len;
+  len = getline(&p->_line, &p->_line_cap, p->stream);
+  if (len == -1) return imp_eof;
+  
+  p->line_num++;
+
+  // remove newline
+  if (p->_line[len-1] == '\n') {
+    p->_line[len-1] = '\0';
+  }
+
+  char *save_ptr;
+  char *token;
+  
+  token = strtok_r(p->_line, " ", &save_ptr);
+
+  // skip empty lines and comments
+  if (token == NULL || token[0] == '#') {
+    return imp_next_command(p);
+  }
+  
+  if (token[0] == '{') {
+    return imp_unexpected_bracket;
+  }
+
+  size_t argc = p->_levels[p->_levels_len - 1];
+  while (token != NULL) {
+    // if there is a comment, skip the rest of the line
+    if (token[0] == '#') break;
+
+    // TODO: check for arguments or characters
+    if (token[0] == '{') {
+      if (p->_levels_len + 1 > p->_levels_cap) {
+        p->_levels_cap *= 2;
+        p->_levels = realloc(p->_levels, sizeof(size_t) * p->_levels_cap);
+      }
+      p->_levels_len++;
+      p->_levels[p->_levels_len-1] = argc;
+      return imp_next_command(p);
+    }
+
+    if (token[0] == '}') {
+      p->_levels_len--;
+      return imp_next_command(p);
+    }
+
+    // add token to argv
+    if (argc + 1 > p->_argv_cap) {
+      if (p->_argv_cap == 0) {
+        p->_argv_cap = 1;
+      }
+      p->_argv_cap *= 2;
+      p->_argv = realloc(p->_argv, sizeof(char*) * p->_argv_cap);
+    }
+
+    p->_argv[argc] = strdup(token);
+    argc++;
+
+    token = strtok_r(NULL, " ", &save_ptr);
+  }
+  
+  p->callback(argc, p->_argv);
+  
+  for (int i=p->_levels[p->_levels_len-1]; i<argc; i++) {
+    free(p->_argv[i]);
+  }
+
+  return imp_success;
+}
---
+++ imp.h
@@ -0,0 +1,44 @@
+// #pragma once
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// TODO: should take payload
+// TODO: should take line number
+typedef void (*imp_command_callback)(size_t argc, char **argv);
+
+enum imp_error {
+  imp_success = 0,
+  imp_eof,
+  imp_unexpected_bracket,
+  // imp_unexpected_token_after_bracket  
+  // for unmatched closing brackets or closing brackets on an empty line
+  // imp_expected_closing_bracket,
+};
+
+extern char *imp_error_str[];
+
+struct imp_parser {
+  imp_command_callback callback;
+  FILE *stream;
+  size_t line_num; // read only
+  
+  // stores the bracket nesting level
+  size_t *_levels;
+  size_t _levels_len;
+  size_t _levels_cap;
+
+  char **_argv;
+  size_t _argv_cap;
+
+  char* _line;
+  size_t _line_cap;
+};
+
+struct imp_parser imp_destroy();
+struct imp_parser imp_init(imp_command_callback callback, FILE* stream);
+enum imp_error imp_next_command(struct imp_parser *p);
+
